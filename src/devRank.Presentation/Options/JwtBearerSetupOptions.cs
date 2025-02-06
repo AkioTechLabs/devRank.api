42970cc61630cfcdf555124d182956806ae49a86
Akio Serizawa <109766532+akiosum@users.noreply.github.com>
2025-02-06 20:18:59 -0300

    ✨ feat: Jwt

--- src/devRank.Presentation/Options/JwtBearerSetupOptions.cs
+++ src/devRank.Presentation/Options/JwtBearerSetupOptions.cs
@@ -13,12 +13,12 @@ public class JwtBearerSetupOptions(IOptions<AuthConfiguracaoDto> options) : IPos
     public void PostConfigure(string? name, JwtBearerOptions optionsSetup)
     {
         optionsSetup.TokenValidationParameters.ValidateIssuer = true;
-        optionsSetup.TokenValidationParameters.ValidIssuer = _options.KeyIssuer;
+        optionsSetup.TokenValidationParameters.ValidIssuer = "TmF0dXJlQnJhYm8";
         optionsSetup.TokenValidationParameters.ValidateAudience = true;
-        optionsSetup.TokenValidationParameters.ValidAudience = _options.KeyAudience;
+        optionsSetup.TokenValidationParameters.ValidAudience = "https://github.com/OrganizacaoTesteSum/devRank.api";
         optionsSetup.TokenValidationParameters.ValidateLifetime = true;
         optionsSetup.TokenValidationParameters.ValidateIssuerSigningKey = true;
         optionsSetup.TokenValidationParameters.IssuerSigningKey =
-            new SymmetricSecurityKey(Encoding.ASCII.GetBytes("TmFvQWRpYW50YU5hb1NlVGFOb0Rldk1ldUlybWFv"));
+            new SymmetricSecurityKey("TmFvQWRpYW50YU5hb1NlVGFOb0Rldk1ldUlybWFv"u8.ToArray());
     }
 }
